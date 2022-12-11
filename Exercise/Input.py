cc217e973cdae3416226c7424c8c1b75feeb526c
xikoo <mahmudsultan125@gmail.com>
2022-12-11 23:26:11 +0600

    Practising Input

---
+++ Exercise/Input.py
@@ -0,0 +1,8 @@
+# input phase
+fname = input("First Name: ")
+lname = input("Last Name: ")
+# process phase
+username = fname + "@" + lname
+# output phase
+print(username)
+print("Username: ", username)
